36d669a6bcd05efb4dbfd09c81b84cc7689db744
MATHEUS COSTA KROSS <matheus.ckross@senacsp.edu.br>
2022-05-27 22:07:06 -0300

    Final Aula 27-05

--- primeiro-site/routes/api.php
+++ primeiro-site/routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(static function(){
+
+    Route::get('/vendedores', [App\Http\Controllers\VendedoresController::class,'index']);
+    Route::post('/vendedores', [App\Http\Controllers\VendedoresController::class,'store']);
+    Route::delete('/vendedores/{id}', [App\Http\Controllers\VendedoresController::class,'destroy']);
+    Route::get('/vendedores/{id}', [App\Http\Controllers\VendedoresController::class,'show']);
+    Route::put('/vendedores/{id}', [App\Http\Controllers\VendedoresController::class,'update']);
+});
+
+
+Route::post('login', [App\Http\Controllers\APIController::class, 'login']);
